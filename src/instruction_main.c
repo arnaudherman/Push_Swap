15fa68f33cd33cdfa607f3a7e4cba0ba7a5162e8
aherman <arnaudherman.ah@gmail.com>
2023-02-09 21:31:47 +0100

    09/02/2023 add instructions go mac

---
+++ src/instruction_main.c
@@ -0,0 +1,122 @@
+void	instructionsf(int num, t_list *stack_a, t_list *stack_b, ...)
+{
+	va_list args;
+	va_start(args, stack_b);
+	char *inst;
+	int size;
+
+	while (num > 0)
+	{	
+		size = stack_size(stack_a);
+		inst = va_arg(args, char *);
+		if (ft_strcmp(inst, "sa") == 0 || ft_strcmp(inst, "sb") == 0|| ft_strcmp(inst, "ss") == 0)
+			sa_sb(stack_a, stack_b, inst);
+		else if (ft_strcmp(inst, "pa") == 0 || ft_strcmp(inst, "pb") == 0)
+		{	
+			pa_pb(stack_a, stack_b, inst);
+			if (ft_strcmp(inst, "pb") == 0)
+			{
+				size--;
+				if (size == 3)
+				{
+					sort_three(stack_a, stack_b);
+					pa_pb(stack_a, stack_b, "pa");
+				}
+				else if (size == 4)
+				{
+					sort_four2(stack_a, stack_b);
+					pa_pb(stack_a, stack_b, "pa");
+					sa_sb(stack_a, stack_b, "sa");		
+				}
+			}
+		}
+		else if (ft_strcmp(inst, "ra") == 0 || ft_strcmp(inst, "rb") == 0
+		|| ft_strcmp(inst, "rr") == 0)
+			ra_rb(stack_a, stack_b, inst);
+		else if (ft_strcmp(inst, "rra") == 0 || ft_strcmp(inst, "rrb") == 0
+		|| ft_strcmp(inst, "rrr") == 0)
+			rra_rrb(stack_a, stack_b, inst);
+		else
+			ft_printf("Error: instruction '%s' does not exist.", inst);	
+		if (check_sort(stack_a) == 0)
+			return ;
+		num--;
+	}
+}
+
+void	instructions(int num, t_list *stack_a, t_list *stack_b, ...)
+{
+	va_list args;
+	va_start(args, stack_b);
+	char *inst;
+
+	while (num > 0)
+	{	
+		inst = va_arg(args, char *);
+		if (ft_strcmp(inst, "sa") == 0 || ft_strcmp(inst, "sb") == 0|| ft_strcmp(inst, "ss") == 0)
+			sa_sb(stack_a, stack_b, inst);
+		else if (ft_strcmp(inst, "pa") == 0 || ft_strcmp(inst, "pb") == 0)
+			pa_pb(stack_a, stack_b, inst);
+		else if (ft_strcmp(inst, "ra") == 0 || ft_strcmp(inst, "rb") == 0
+		|| ft_strcmp(inst, "rr") == 0)
+			ra_rb(stack_a, stack_b, inst);
+		else if (ft_strcmp(inst, "rra") == 0 || ft_strcmp(inst, "rrb") == 0
+		|| ft_strcmp(inst, "rrr") == 0)
+			rra_rrb(stack_a, stack_b, inst);
+		else
+			ft_printf("Error: instruction '%s' does not exist.", inst);	
+		num--;
+	}
+}
+
+
+void	sa_sb(t_list *stack_a, t_list *stack_b, char *inst)
+{
+	if (ft_strcmp(inst, "sa") == 0)
+		swap(stack_a);
+	else if (ft_strcmp(inst, "sb") == 0)
+		swap(stack_b);
+	else if (ft_strcmp(inst, "ss") == 0)
+	{
+		swap(stack_a);
+		swap(stack_b);
+	}
+	ft_printf("%s\n", inst);
+}
+
+void	pa_pb(t_list *stack_a, t_list *stack_b, char *inst)
+{
+	if (ft_strcmp(inst, "pa") == 0)
+		push(stack_a, stack_b);
+	else if (ft_strcmp(inst, "pb") == 0)
+		push(stack_b, stack_a);
+	ft_printf("%s\n", inst);
+}
+
+void ra_rb(t_list *stack_a, t_list *stack_b, char *inst)
+{
+	if (ft_strcmp(inst, "ra") == 0)
+		rotate(stack_a);
+	else if (ft_strcmp(inst, "rb") == 0)
+		rotate(stack_b);
+	else if (ft_strcmp(inst, "rr") == 0)
+	{
+		rotate(stack_a);
+		rotate(stack_b);
+	}
+	ft_printf("%s\n", inst);
+}
+
+void	rra_rrb(t_list *stack_a, t_list *stack_b, char *inst)
+{
+	if (ft_strcmp(inst, "rra") == 0)
+		reverse(stack_a);
+	else if (ft_strcmp(inst, "rrb") == 0)
+		reverse(stack_b);
+	else if (ft_strcmp(inst, "rrr") == 0)
+	{
+		reverse(stack_a);
+		reverse(stack_b);
+	}
+	ft_printf("%s\n", inst);
+}
---
+++ src/instruction_utils.c
@@ -0,0 +1,90 @@
+void swap(t_list *stack)
+{
+    t_element *temp;
+
+    if (!stack->first || !stack->first->nxt)
+        return;
+    temp = stack->first;
+    stack->first = stack->first->nxt;
+    temp->nxt = stack->first->nxt;
+    stack->first->prev = NULL;
+    if (temp->nxt)
+        temp->nxt->prev = temp;
+    else
+        stack->last = temp;
+    stack->first->nxt = temp;
+    temp->prev = stack->first;
+}
+
+
+void push(t_list *dest, t_list *src) 
+{
+	 t_element *temp;
+
+    if (!src->first)
+        return;
+    temp = src->first;
+    if (src->first == src->last) 
+        src->first = src->last = NULL;
+    else 
+    {
+        src->first = temp->nxt;
+        src->first->prev = NULL;
+    }
+    temp->nxt = dest->first;
+    temp->prev = NULL;
+    if (dest->first)
+        dest->first->prev = temp;
+    dest->first = temp;
+    if (!dest->last)
+        dest->last = temp;
+}
+
+
+void	rotate(t_list *stack)
+{
+	t_element	*temp;
+	
+	if (!stack->first && !stack->first->nxt)
+		return ;
+	temp = stack->first;
+	stack->first = stack->first->nxt;
+	stack->first->prev = NULL;
+	temp->prev = stack->last;
+	temp->nxt = NULL;
+	stack->last->nxt = temp;
+	stack->last = temp;
+}
+
+void	reverse(t_list *stack)
+{
+	t_element	*temp;
+	
+	if (!stack->first || !stack->first->nxt)
+		return ;
+	temp = stack->last;
+	stack->last = stack->last->prev;
+	stack->last->nxt = NULL;
+	temp->nxt = stack->first;
+	temp->prev = NULL;
+	stack->first->prev = temp;
+	stack->first = temp;
+}
+
+/* void   reverse(t_list *stack)
+{
+    t_element    *temp_first;
+    t_element    *temp_last;
+    t_element    *temp_second;
+    if (stack->first == NULL || stack->last == NULL)
+        return ;
+    temp_first = stack->first;
+    temp_last = stack->last;
+    temp_second = temp_first->nxt;
+    stack->first = temp_second;
+    temp_second->prev = NULL;
+    stack->last = temp_first;
+    temp_first->nxt = NULL;
+    temp_first->prev = temp_last;
+    temp_last->nxt = stack->first;
+} */
