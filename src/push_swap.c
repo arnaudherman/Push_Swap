a2ab493a817702c46bd13dc50bd9abceee672603
aherman <arnaudherman.ah@gmail.com>
2023-02-09 20:49:30 +0100

    09/02/2023 moov to linux push

---
+++ src/push_swap.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push_swap.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aherman <aherman@student.42lausanne.ch>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/09 20:47:29 by aherman           #+#    #+#             */
+/*   Updated: 2023/02/09 20:47:29 by aherman          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "pushswap.h"
+
+int	main(int argc, char **argv)
+{
+	t_list	*stack_a;
+	t_list	*stack_b;
+
+	stack_a = malloc(sizeof(t_list));
+	stack_b = malloc(sizeof(t_list));
+	check_args(argc, argv, stack_a);
+	get_position(stack_a);
+	caca_sort_ex(stack_a, stack_b);
+}
