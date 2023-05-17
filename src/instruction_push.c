daeab58c37d147f881e0458b97e94e26d2de1d3c
aherman <aherman@student.42lausanne.ch>
2023-05-17 20:18:11 +0200

    need rework all instruction

--- src/instruction_push.c
+++ src/instruction_push.c
@@ -6,7 +6,7 @@
 /*   By: aherman <aherman@student.42lausanne.ch>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/13 13:58:57 by aherman           #+#    #+#             */
-/*   Updated: 2023/05/09 12:03:13 by aherman          ###   ########.fr       */
+/*   Updated: 2023/05/17 19:30:13 by aherman          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,6 +37,32 @@ void	push(t_list *dest, t_list *src)
 	if (!dest->last)
 		dest->last = temp;
 }
+//A re-faire need fix 2x malloc
+// void	push(t_list *dest, t_list *src)
+// {
+// 	t_element	*temp;
+
+// 	temp = src->first;
+// 	if (temp->nxt != NULL)
+// 	{
+// 		src->first = temp->nxt;
+// 		src->first->prev = NULL;
+// 	}
+// 	else
+// 	{
+// 		temp->nxt = NULL;
+// 		src->first = NULL;
+// 		src->last = NULL;
+// 	}
+// 	if (dest->first != NULL)
+// 	{
+// 		temp->nxt = dest->first;
+// 		temp->nxt->prev = temp;
+// 	}
+// 	else
+// 		dest->last = temp;
+// 	dest->first = temp;
+// }
 
 void	pa_pb(t_list *stack_a, t_list *stack_b, char *inst)
 {
